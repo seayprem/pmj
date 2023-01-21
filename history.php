4dd1746fea21d1fa3d301b8518640fec40041a6d
SeayPrem <seayprem@hotmail.com>
2023-01-22 00:50:01 +0700

    update before sleep

---
+++ history.php
@@ -0,0 +1,185 @@
+<?php 
+include('config/db.php');
+session_start();
+// Check Session
+if(empty($_SESSION['emp_role'])) {
+  // header("Location: login.php");
+  echo '<script src="js/sweetalert2@11.js"></script>';
+  echo '<script src="js/jquery-3.6.3.min.js"></script>';
+  // echo '<script>window.location = "login.php"</script>';
+  echo "<script>
+  $(document).ready(function() {
+    $('div').hide();
+    Swal.fire({
+      icon: 'error',
+      title: 'คุณไม่ได้รับอนุญาตในการเข้าถึงหน้าต่างนี้',
+      text: 'กรุณาเข้าสู่ระบบ!',
+    }).then((result) => {
+      window.location.href = 'login.php';
+    });
+  });
+  </script>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ประวัติข้อมูลการเบิกวัสดุสำนักงาน | สํานักงานพัฒนาสังคมและความมั่นคงของมนุษย์ นครราชสีมา</title>
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/fonts.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/fontawesome.min.css">
+
+  <style>
+    .dataTables_filter input {
+      margin-bottom: 1em;
+    }
+  </style>
+
+</head>
+<body>
+  
+  <div class="main-container d-flex">
+    <!-- start navbar  -->
+    <div class="sidebar" id="side_nav">
+      <div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
+        <h1 class="fs-4"><span class="bg-white text-dark rounded shadow px-2 me-2"></span><span class="text-white">สนง. พมจ.</span></h1>
+        <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i
+            class="fa-solid fa-bars-staggered"></i></button>
+      </div>
+
+      <!-- Start for menuSidebar  -->
+      <?php include('includes/menuSidebar.inc.php'); ?>
+      <!-- End for menuSidebar  -->
+
+    </div>
+
+    <!-- end navbar  -->
+
+    <!-- start content -->
+    <div class="content">
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid">
+
+          <div class="d-flex justify-content-between d-md-none d-block">
+            <button class="btn px-1 py-0 open-btn me-2"><i class="fa-solid fa-bars-staggered"></i></button>
+            <a class="navbar-brand" href="#"><span class="bg-pmj rounded px-2 py-0 text-white">สนง. พมจ.</span></a>
+          </div>
+
+          <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars-staggered"></i>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- end content -->
+
+    <!-- start dashboard content  -->
+    <div class="dashboard-content px-3 pt-4">
+      <h2 class="fs-5"> ประวัติข้อมูลการเบิกวัสดุสำนักงาน</h2>
+      <hr>
+      
+     <!-- Start List tranfer only Supllier -->
+      <table class="table" id="myTable">
+        <thead>
+          <th class="text-center">ลำดับ</th>
+          <th class="text-center">ประเภทการทำรายการ</th>
+          <th class="text-center">สถานะ</th>
+          <th class="text-center">เวลาทำรายการ</th>
+          <th class="text-center">รายละเอียด</th>
+        </thead>
+        <tbody>
+          <?php 
+          $emp_id = $_SESSION['emp_id'];
+          $i = 0;
+          $sql = "SELECT * FROM `transfer` INNER JOIN status ON transfer.stat_id = status.stat_id WHERE transfer.emp_id = $emp_id ORDER BY transfer.t_id DESC";
+          $query = mysqli_query($conn, $sql);
+          while($row = mysqli_fetch_array($query)) {
+
+            $i++;
+          
+          ?>
+          <tr class="text-center">
+            <td><?= $i; ?></td>
+            <td>
+              <?php if($row['t_type'] == 1) {
+                echo 'เบิกวัสดุสำนักงาน';
+              } else {
+                echo 'นำเข้า';
+              } ?>
+            </td>
+            <td>
+              <?php if($row['stat_status'] == 1) {
+                echo 'รอการอนุมัติ';
+              } else {
+                echo 'ไม่อนุมัติ';
+              } ?>
+            </td>
+            <td><?= $row['t_datetime']; ?></td>
+            <td>
+              <a href="history_detail.php?id=<?= $row['t_id']; ?>" class="btn btn-secondary">รายละเอียด</a>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+     <!-- End List tranfer only Supllier -->
+
+    </div>
+
+
+    <br><br><br>
+    <!-- end dashboard content  -->
+
+  </div>
+  
+
+  
+
+  <script src="js/jquery-3.6.3.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/sweetalert2@11.js"></script>
+  <script src="js/jquery.dataTables.min.js"></script>
+  <script src="js/responsive.js"></script>
+
+  <script>
+    $(document).ready(function() {
+
+      // Start List Data Table 
+      $('#myTable').DataTable();
+      // End List Data Table 
+
+      $('#logout').on('click', function(e) {
+        e.preventDefault();
+
+        Swal.fire({
+          icon: 'success',
+          title: 'ออกจากระบบสำเร็จ',
+          showConfirmButton: false,
+          timer: 1500
+        }).then((result) => {
+          window.location = 'logout.php';
+        })
+      })
+
+    })
+  </script>
+
+</body>
+</html>
---
+++ history_detail.php
@@ -0,0 +1,211 @@
+<?php 
+include('config/db.php');
+session_start();
+// Check Session
+if(empty($_SESSION['emp_role'])) {
+  // header("Location: login.php");
+  echo '<script src="js/sweetalert2@11.js"></script>';
+  echo '<script src="js/jquery-3.6.3.min.js"></script>';
+  // echo '<script>window.location = "login.php"</script>';
+  echo "<script>
+  $(document).ready(function() {
+    $('div').hide();
+    Swal.fire({
+      icon: 'error',
+      title: 'คุณไม่ได้รับอนุญาตในการเข้าถึงหน้าต่างนี้',
+      text: 'กรุณาเข้าสู่ระบบ!',
+    }).then((result) => {
+      window.location.href = 'login.php';
+    });
+  });
+  </script>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ประวัติข้อมูลการเบิกวัสดุสำนักงาน | สํานักงานพัฒนาสังคมและความมั่นคงของมนุษย์ นครราชสีมา</title>
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/fonts.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="css/all.min.css">
+  <link rel="stylesheet" href="css/fontawesome.min.css">
+
+  <style>
+    .dataTables_filter input {
+      margin-bottom: 1em;
+    }
+  </style>
+
+</head>
+<body>
+  
+  <div class="main-container d-flex">
+    <!-- start navbar  -->
+    <div class="sidebar" id="side_nav">
+      <div class="header-box px-2 pt-3 pb-4 d-flex justify-content-between">
+        <h1 class="fs-4"><span class="bg-white text-dark rounded shadow px-2 me-2"></span><span class="text-white">สนง. พมจ.</span></h1>
+        <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i
+            class="fa-solid fa-bars-staggered"></i></button>
+      </div>
+
+      <!-- Start for menuSidebar  -->
+      <?php include('includes/menuSidebar.inc.php'); ?>
+      <!-- End for menuSidebar  -->
+
+    </div>
+
+    <!-- end navbar  -->
+
+    <!-- start content -->
+    <div class="content">
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container-fluid">
+
+          <div class="d-flex justify-content-between d-md-none d-block">
+            <button class="btn px-1 py-0 open-btn me-2"><i class="fa-solid fa-bars-staggered"></i></button>
+            <a class="navbar-brand" href="#"><span class="bg-pmj rounded px-2 py-0 text-white">สนง. พมจ.</span></a>
+          </div>
+
+          <button class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars-staggered"></i>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#"></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- end content -->
+
+    <!-- start dashboard content  -->
+    <div class="dashboard-content px-3 pt-4">
+      <h2 class="fs-5"> รายละเอียดประวัติข้อมูลการเบิกวัสดุสำนักงาน</h2>
+      <hr>
+
+      <!-- start show status  -->
+      <?php 
+      $status_sql = "SELECT * FROM `transfer` INNER JOIN status ON transfer.stat_id = status.stat_id LEFT JOIN employees ON status.emp_id = employees.emp_id";
+      $status_query = mysqli_query($conn, $status_sql);
+      $status_row = mysqli_fetch_array($status_query);
+      ?>
+      <h2 class="fs-5 text-center">สถานะ: 
+        <?php 
+        if($status_row['stat_status'] == 1) {
+          echo 'รอการอนุมัติ';
+        } else if($status_row['stat_status'] == 2) {
+          echo 'อนุมัติ';
+        } else {
+          echo 'ไม่อนุมัติ';
+        }
+        ?>
+      </h2>
+      <h2 class="fs-5 text-center"> 
+      <?php 
+        if($status_row['stat_reason'] == "") {
+          echo 'หมายเหตุ: ยังไม่ได้ระบุ';
+        } else {
+          echo 'หมายเหตุ: ' . $status_row['stat_reason'];
+        }
+      ?>
+      </h2>
+      <h2 class="fs-5 text-center"> 
+      <?php 
+        if($status_row['emp_id'] == 0) {
+          echo 'ผู้รับผิดชอบในการอนุมัติ: ยังไม่ได้ระบุ';
+        } else {
+          echo 'ผู้รับผิดชอบในการอนุมัติ: ' . $status_row['emp_fname'] . ' ' . $status_row['emp_lname'];
+        }
+      ?>
+      </h2>
+      <!-- end show status  -->
+      
+     <!-- Start List tranfer only Supllier -->
+     
+     <table class="table" id="myTable">
+      <thead>
+        <th class="text-center">ลำดับ</th>
+        <th class="text-center">รายการวัสดุสำนักงาน</th>
+        <th class="text-center">จำนวน</th>
+      </thead>
+      <tbody>
+          <?php 
+        if(isset($_GET['id'])) {
+          $id = $_GET['id'];
+          $sql = "SELECT * FROM transfer_detail INNER JOIN offices ON transfer_detail.office_id = offices.office_id WHERE t_id = $id";
+          $query = mysqli_query($conn, $sql);
+          $i = 0;
+          while($row = mysqli_fetch_array($query)) {
+            $i++;
+         
+        ?>
+        <tr>
+          <td class="text-center"><?= $i; ?></td>
+          <td class="text-center"><?= $row['office_name']; ?></td>
+          <td class="text-center"><?= $row['tdel_qty']; ?></td>
+        </tr>
+        <?php  }
+        } ?>
+      </tbody>
+     </table>
+     <div class="container">
+      <div class="text-center mt-3">
+        <a href="history.php" class="btn btn-danger">ย้อนกลับ</a>
+      </div>
+     </div>
+     <!-- End List tranfer only Supllier -->
+
+    </div>
+
+
+    <br><br><br>
+    <!-- end dashboard content  -->
+
+  </div>
+  
+
+  
+
+  <script src="js/jquery-3.6.3.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/sweetalert2@11.js"></script>
+  <script src="js/jquery.dataTables.min.js"></script>
+  <script src="js/responsive.js"></script>
+
+  <script>
+    $(document).ready(function() {
+
+      // Start List Data Table 
+      $('#myTable').DataTable();
+      // End List Data Table 
+
+      $('#logout').on('click', function(e) {
+        e.preventDefault();
+
+        Swal.fire({
+          icon: 'success',
+          title: 'ออกจากระบบสำเร็จ',
+          showConfirmButton: false,
+          timer: 1500
+        }).then((result) => {
+          window.location = 'logout.php';
+        })
+      })
+
+    })
+  </script>
+
+</body>
+</html>
